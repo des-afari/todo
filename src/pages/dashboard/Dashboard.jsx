368d622b002c5500e96097f162c618efa6b80ce7
Desmond Afari <67256731+Reeler@users.noreply.github.com>
2022-05-14 18:41:00 +0100

    continue

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from '../../components/header/Header'
 import './dashboard.css'
 import { ReactComponent as CheckBox } from '../../assets/svg/checkbox.svg'
+import { ReactComponent as Left } from '../../assets/svg/left.svg'
+import { ReactComponent as Right } from '../../assets/svg/right.svg'
 
 const Dashboard = () => {
   return (
@@ -35,12 +37,34 @@ const Dashboard = () => {
                         <CheckBox />
                     </span>
                     <span>
-                        <h3>Go to school</h3>
+                        <h3>Go to Church</h3>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt pariatur nisi numquam ad iure voluptas voluptate facilis aspernatur quo cupiditate, in reprehenderit alias ducimus odit magni? Ipsa quam rerum sint!</p>
                     </span>
                 </li>
             </ul>
-            <div className='dashboard__three'></div>
+            <ul className='dashboard__three'>
+                <div className='choose__month'>
+                    <span>
+                        <Left />
+                    </span>
+                    <span>
+                        <h3>September</h3>
+                    </span>
+                    <span>
+                        <Right />
+                    </span>
+                </div>
+                <ul className='choose__date'>
+                    <li className='date__item'>
+                        <span>
+                            <h1>31</h1>
+                        </span>
+                        <span>
+                            <p>Monday</p>
+                        </span>
+                    </li>
+                </ul>
+            </ul>
         </div>
     </>
   )
